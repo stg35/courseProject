41dfee9000de5df7bda3cccdbf888a9dd9a4418a
Alex <alex30052003@icloud.com>
2022-06-28 13:57:47 +0300

    with tests

--- ClientMessenger/build-m_tests-Qt_6_2_4_for_macOS-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/stg/C++/CourseProject/ClientMessenger/m_tests
-# Build directory: /Users/stg/C++/CourseProject/ClientMessenger/build-m_tests-Qt_6_2_4_for_macOS-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
